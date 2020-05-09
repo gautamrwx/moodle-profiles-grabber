fea8eafd717158fd93e20a95bd0ffd0b40dc9ceb
gautamrwx <gautam.404@protonmail.com>
2020-05-09 11:14:04 +0530

    Made It User Interractive

--- program.py
+++ program.py
@@ -2,26 +2,31 @@ from selenium import webdriver
 import sqlite3
 
 conn = sqlite3.connect('moodledata.db')
+print("Database Connection Ok")
 
-print("Connection Ok")
+# Get All The Necessary Information From User
+print("\n ==== [ Welcome To MOODLE Information Grabber ] ===\n")
+
+login_url = input("(Step 1 of 5) Enter Url Of MoLogin Page : ")
+
+username    = input("(Step 2 of 5) Username : ")
+password    = input("(Step 3 of 5) Password : ")
+
+initial_id = int(input('(Step 4 of 5) Enter The Starting Profile Id : '))
+final_id = int(input('(Step 5 of 5) Enter The Terminating Profile Id : '))
+
+print("Entry Done ...")
+
+# Open Browser And Load Url
 driver = webdriver.Chrome()
+driver.get(login_url)
 
-# login to moodle
-driver.get('http://moodlebppimt.ddns.net/login/index.php')
-
-#set Usename And Password For Moodle
-username    =''
-password    =''
-
+# Login To Moodle
 driver.find_element_by_xpath("//input[@id='username']").send_keys(username)
 driver.find_element_by_xpath("//input[@id='password']").send_keys(password)
 driver.find_element_by_xpath("//button[@id='loginbtn']").click()
 
-
-#Fetch Information
-initial_id = int(input('Enter The Id Of Starting Point : '))
-final_id = int(input('Enter The Id Of Stopping Point : '))
-
+# Start Data Grabbing
 for i in range(initial_id,final_id):
     driver.get('http://moodlebppimt.ddns.net/user/profile.php?id='+str(i))
 
